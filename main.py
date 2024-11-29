6ba954d35b60ddeca1d825e83ff6ab76fe398e25
ruzgarizbudak <izbudakruzgar@gmail.com>
2024-11-29 23:42:30 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,137 @@
-# İçeri Aktarma
+# İçe aktar
 from flask import Flask, render_template,request, redirect
-# Veritabanı kütüphanesini içe aktarma
+# Veri tabanı kitaplığını bağlama
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-# SQLite ile bağlantı kurma 
+# SQLite'ı bağlama
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///diary.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# DB oluşturma
-db = SQLAlchemy(app )
+# Veri tabanı oluşturma
+db = SQLAlchemy(app)
+# Tablo oluşturma
 
-#Görev #1. DB tablosu oluşturma
 class Card(db.Model):
-    title=db.Column(db.String(13),nullable=False)
-    subtitle=db.Column(db.String(50),nullable=False)
-    text=db.Column(db.String(300),nullable=False)
-    id=db.Column(db.Integer,nullable=False,primary_key=True)
+    # Sütun oluşturma
+    # id
+    id = db.Column(db.Integer, primary_key=True)
+    # Başlık
+    title = db.Column(db.String(100), nullable=False)
+    # Tanım
+    subtitle = db.Column(db.String(300), nullable=False)
+    # Metin
+    text = db.Column(db.Text, nullable=False)
+
+    # Nesnenin ve kimliğin çıktısı
     def __repr__(self):
         return f'<Card {self.id}>'
+    
 
 
-
-
-
-
-
-
+class User (db.Model):
+    email=db.Column(db.String(50),nullable=False)
+    password=db.Column(db.String(30),nullable=False)
+    id=db.Column(db.Integer,primary_key=True,autoincrement=True)
 
 
 
 # İçerik sayfasını çalıştırma
-@app.route('/')
-def index():
-    # DB nesnelerini görüntüleme
-    # Görev #2. DB'deki nesneleri index.html'de görüntüleme
-    kartlar=Card.query.all()
+@app.route('/', methods=['GET','POST'])
+def login():
+        error = ''
+        if request.method == 'POST':
+            form_login = request.form['email']
+            form_password = request.form['password']
+            
+            #Ödev #4. yetkilendirmeyi uygulamak
+            db_users=User.query.all()
+            for user in db_users:
+                if form_login==user.email and form_password== user.password:
+                    return redirect('/index')
+            else:
+                error = 'Bir işi de düzgün yap!!'
+                return render_template('login.html', error=error)
+
+
+            
+        else:
+            return render_template('login.html')
+
+
+
+@app.route('/reg', methods=['GET','POST'])
+def reg():
+    hata=''
+    if request.method == 'POST':
+        login= request.form['email']
+        password = request.form['password']
+        if len(password)>=8 and len(password)<20:
+            user=User(email=login,password=password)
+            db.session.add(user)
+            db.session.commit()
+            return redirect('/')
+        else:
+            hata='Şifre uzunluğun 8 ila 20 arası olmalı'
+            return render_template('/registration.html',hata=hata)
     
+    else:    
+        return render_template('registration.html',hata=hata)
 
-    return render_template('index.html',
-                           cards=kartlar
 
-                           )
+# İçerik sayfasını çalıştırma
+@app.route('/index')
+def index():
+    # Veri tabanı girişlerini görüntüleme
+    cards = Card.query.order_by(Card.id).all()
+    return render_template('index.html', cards=cards)
 
-# Kartla sayfayı çalıştırma
+# Kayıt sayfasını çalıştırma
 @app.route('/card/<int:id>')
 def card(id):
-    # Görev #2. Id'ye göre doğru kartı görüntüleme
-    card=Card.query.get(id)
+    card = Card.query.get(id)
 
     return render_template('card.html', card=card)
 
-# Sayfayı çalıştırma ve kart oluşturma
+# Giriş oluşturma sayfasını çalıştırma
 @app.route('/create')
 def create():
     return render_template('create_card.html')
 
-# Kart formu
+# Giriş formu
 @app.route('/form_create', methods=['GET','POST'])
 def form_create():
     if request.method == 'POST':
         title =  request.form['title']
         subtitle =  request.form['subtitle']
-        metin =  request.form['text']
+        text =  request.form['text']
 
-        # Görev #2. Verileri DB'de depolamak için bir yol oluşturma
-        ege=Card(title=title,subtitle=subtitle,text=metin)
-        db.session.add(ege)
+        # Veri tabanına gönderilecek bir nesne oluşturma
+        card = Card(title=title, subtitle=subtitle, text=text)
+
+        db.session.add(card)
         db.session.commit()
-
-
-
-        return redirect('/')
+        return redirect('/index')
     else:
         return render_template('create_card.html')
     
+
 @app.route('/delete/<int:id>')
 def delete(id):
     card=Card.query.get(id)
-    if card:#Öğretmenim nolur nolmaz diye ekledim bunu 
+    if card:
         db.session.delete(card)
         db.session.commit()
-    return render_template('index.html')
+    return redirect ('/')
+
+
+
+
+
 
 
 
 if __name__ == "__main__":
     with app.app_context():
-         db.create_all()
+        db.create_all()
     app.run(debug=True)
